09418970641f8026649930d8274249c2210ab285
Sagi Evroni <94626655+SagiEv@users.noreply.github.com>
2025-12-19 14:06:56 +0200

    added current-user feature (#66)

--- backend/src/main/java/com/swipelab/dto/response/UserProfileResponse.java
+++ backend/src/main/java/com/swipelab/dto/response/UserProfileResponse.java
@@ -1,5 +1,16 @@
 package com.swipelab.dto.response;
 
+import com.swipelab.model.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class UserProfileResponse {
-    
+
+    private String email;
+    private String username;
+    private UserRole role;
 }
